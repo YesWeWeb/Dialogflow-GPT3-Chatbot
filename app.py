3233f3772166b4305f053187af258d621a4fe6bd
YesWeWeb <greg@yesweweb.fr>
2023-02-15 20:19:34 -0400

    o

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def get_gpt3_response(final_prompt):
 		engine="text-davinci-003",
 		prompt=f"{final_prompt}\n",
 		temperature=0.5,
-		max_tokens=1024,
+		max_tokens=500,
 		top_p=1,
 		frequency_penalty=0,
 		presence_penalty=0)
