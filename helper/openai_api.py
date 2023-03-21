0b533dc9edd2bd07c9075d1a76b70e99fde4f5b9
YesWeWeb <greg@yesweweb.fr>
2023-03-21 14:57:28 -0400

    ok

--- helper/openai_api.py
+++ helper/openai_api.py
@@ -1,12 +1,9 @@
 import os
-
-import openai
 from dotenv import load_dotenv
+import openai
+
 load_dotenv()
-
-
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
+openai.api_key = os.environ.get('OPENAI_KEY')
 
 def text_complition(prompt: str) -> dict:
     '''
